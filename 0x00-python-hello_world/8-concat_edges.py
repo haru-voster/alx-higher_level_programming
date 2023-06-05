e67b9705c72561bea8614545c73518c45deef26e
haru-voster <Harounbravo2@gmail.com>
2023-06-05 08:17:03 -0700

    OOPS with newline

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[36:66] + str[100:110] + str[0:6]
+print(str)
