2d2fdd2f0f1b88d5e01e9e66183c522ce26ed9c0
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-06 22:58:13 +0300

    hexade

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
+for num in range(0, 99):
+    print('{} = 0x{:x}'.format(num, num))
