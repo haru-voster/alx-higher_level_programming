a7533b6b8822868f568e68968c5429420e1720e7
haru-voster <Harounbravo2@gmail.com>
2023-06-08 05:49:31 -0700

    addition operation

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def add():
+"""addition operation"""
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    print(" the numbers", a, "++", b, "=", add(a, b))
+    print("{} + {} = {}".format(a, b, add(a, b)))
