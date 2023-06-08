3fa3b4b65c2a2a0c8b623dd940f7b7d71c8adb18
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-08 22:31:31 +0300

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# 5-variable load
+#!/usr/bin/python3
 if __name__ == "__main__":
-    from variable_load_5 import a
-
-    print(a)
+    import variable_load_5
+    print(variable_load_5.a)
