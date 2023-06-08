1ef18ad34a4d057998bd839420a29089e59677bb
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-08 22:15:50 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-import sys
-
-def print_arguments(argv):
-    num_arg = len(argv)
-    print(f"Number of argument(s): {num_arg}", end='')
-    if num_arg == 0:
-        print(".", end='\n')
-    else:
-        if num_arg == 1:
-            print(":")
-            print(f"1: {argv[0]}")
-        else:
-            print("s:")
-            for i in range(num_arg):
-                print(f"{i+1}: {argv[i]}")
+# 2-args.py
+# haru-voster
 
 if __name__ == "__main__":
-    print_arguments(sys.argv[1:])
+    """Print the number of and list of arguments."""
+    import sys
 
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
