07593eef5d8f97195345e8338746e43a2c3e245c
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-08 20:59:34 +0300

    number/list of arg

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 2-args.py
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {:s}".format(argv[1]))
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
