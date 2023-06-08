a4be6b4118db12e1082f5a0906bebb74a7328c4d
haru-voster <Harounbravo2@gmail.com>
2023-06-08 05:37:45 -0700

    Addition of operations

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,6 @@
 """addition operation"""
 if __name__ == "__name__":
     from add_0 import add
-    a=1
-    b=2
-
-    print("{} + {} = {}".format(a, b, add(a,b)))
-
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
