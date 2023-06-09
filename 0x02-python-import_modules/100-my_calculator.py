b3af33b1b70e80e350f114a295e674e7b0c428ef
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-09 05:58:24 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
-    exit()
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
 
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-elif argv[2] == '+':
-    func = add
-elif argv[2] == '-':
-    func = sub
-elif argv[2] == '*':
-    func = mul
-elif argv[2] == '/':
-    func = div
-else:
-    print("Unknown operator. Available operators: +, -, *, and /")
-    exit(1)
+    ac = len(argv) - 1
 
-results = func(int(argv[1]), int(argv[3]))
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
-    argv[2], int(argv[3]), results))
+    if ac != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if argv[2] == '+':
+        sum = add(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {:d}".format(argv[1], argv[2], argv[3], sum))
+    elif argv[2] == '-':
+        difr = sub(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {:d}".format(argv[1], argv[2], argv[3], difr))
+    elif argv[2] == '*':
+        prod = mul(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {:d}".format(argv[1], argv[2], argv[3], prod))
+    elif argv[2] == '/':
+        quot = div(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {:d}".format(argv[1], argv[2], argv[3], quot))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    exit(0)
