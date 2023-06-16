0bebc3288bd2151d8f0c3baa79d0953d2285f4b9
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-16 03:41:13 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,38 +2,74 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - inserts a node at position
+ * create_dnode - creates a new DLL node with data
+ * @n: data to add to node
+ *
+ * Return: pointer to newly allocated/populated node
+ */
+dlistint_t *create_dnode(const int n)
+{
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->n = n;
+
+	return (new_node);
+}
+
+/**
+ * insert_dnodeint_at_index - inserts a node at position
  * in a doubly linked list
  * @h: double pointer to the head, so we can modify if needed
- * @index: index to insert new node at
+ * @idx: index to insert new node at
+ * @n: data to add to new node
  *
- * Return: 1 (success), -1 (failure)
+ * Return: pointer to new element, NULL on failure.
  */
-int delete_dnodeint_at_index(dlistint_t **h, unsigned int index)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp = NULL;
-	unsigned int x = 0;
+	dlistint_t *new_node = NULL, *temp = NULL;
+	unsigned int i = 0;
 
+	new_node = create_dnode(n);
+	if (!new_node)
+		return (NULL);
 	if (!h || !(*h)) /* NULL DLL */
-	{
-		return (-1);
-	}
+		*h = new_node;
 	else /* DLL exists */
 	{
 		temp = *h;
 		/* advance to pos of idx in DLL */
-		while (index != x++ && temp)
+		while (idx != i++ && temp->next)
 			temp = temp->next;
-		if (!temp) /* end of DLL */
-			return (-1);
 		if (temp->next)
-			temp->next->prev = temp->prev;
-		if (index == 0) /* delete at head */
-			*h = temp->next;
+			new_node->prev = temp->prev;
 		else
-			temp->prev->next = temp->next;
-		free(temp);
-		return (1);
+			new_node->prev = temp;
+		if (idx == i) 
+		{
+			temp->next = new_node;
+			new_node->prev = temp;
+		}
+		else if (idx == i - 1) /* insert at head or middle */
+		{
+			if (temp->prev)
+				temp->prev->next = new_node;
+			else /* head of LL */
+				*h = new_node;
+			temp->prev = new_node;
+			new_node->next = temp;
+		}
+		else /*  range */
+		{
+			free(new_node);
+			return (NULL);
+		}
 	}
-	return (-1);
+	return (new_node);
 }
