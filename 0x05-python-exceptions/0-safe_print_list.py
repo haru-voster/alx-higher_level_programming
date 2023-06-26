3a724d558fe10049a40cd4c6cfd0724f5514cfe0
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-26 20:18:33 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    a = 0
     printed_number = 0
-    for a in range(0, x):
+    for a in range(x):
         try:
             print("{}".format(my_list[a]), end="")
             printed_number += 1
         except Error:
             continue
-    print()
+    print("")
     return printed_number
