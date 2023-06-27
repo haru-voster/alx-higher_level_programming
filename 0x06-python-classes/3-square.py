b837fff3430b6c3a24768cf11127e4873d6f3973
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-27 22:17:56 +0300

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """defineing  a square object"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """calculates and returns the current area"""
+        return self.__size * self.__size
