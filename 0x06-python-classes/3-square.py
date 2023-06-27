c3e466af5bfd4efa80e168abcdebc300a89ed284
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-27 22:18:59 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
+"""Module that includes Square class"""
 
 class Square:
-    """defineing  a square object"""
+    """defines a square object"""
     def __init__(self, size=0):
+        """initializes the size"""
         self.__size = size
         if type(size) == int:
             if size < 0:
