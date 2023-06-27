880caa35bee405dba6c93c8933a8463242705a15
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-27 22:28:36 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Module with a class thet defines a square"""
+"""defines a square """
 
 
 class Square:
-    """Defining a square object
-    """
-    def __init__(self, size):
-        """Initializes the size
-
-        Args:
-            param1 (int): square size
-        """
+    """A Class That Defines a Square"""
+    def __init__(self, size=0):
+        """initializes the size"""
         self.__size = size
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
