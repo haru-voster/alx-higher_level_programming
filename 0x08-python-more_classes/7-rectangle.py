9021185d3a14dca366888ef3b6328d5c07e77c64
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-03 19:50:37 +0300

    Update 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -4,11 +4,31 @@
 
 
 class Rectangle:
-     number_of_instances = 0
+    """Class for printing or computation of dimensions of a rectangle.
+
+    Takes in args for width and height of a rectangle, and contains methods
+    for calculation of the area or perimeter. __str__, __repr__, and __del__
+    fuctionality defined below.
+
+    Attributes:
+        number_of_instances (int): counter incrementing for every
+            instantiation, and decrementing for every instance deletion.
+        print_symbol (str): single character to be used in assembling string
+            representation of rectangle
+
+    """
+    number_of_instances = 0
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-       
+        """Increments `number_of_instances` and calls setters for `__width`
+        and `__height`.
+
+        Args:
+            width (int): horizontal dimension of rectangle, defaults to 0
+            height (int): vertical dimension of rectangle, defaults to 0
+
+        """
         type(self).number_of_instances += 1
         # attribute assigment here engages setters defined below
         self.width = width
