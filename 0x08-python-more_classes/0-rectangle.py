247fd65eda78bae32311691d6e098e5604b3a313
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-03 19:10:31 +0300

    rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0-rectangle, built by haroun task0.
+"""
+
+
+class Rectangle:
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
+    pass
