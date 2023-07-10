b920b742d532dc9c77e535d56a16a907606ef585
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-10 21:20:37 +0300

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+3-is_kind_of_class.py
+haru-voster
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ return True if an object is an instance of a class
+    that inherited from"""
+
+    return isinstance(obj, a_class)
