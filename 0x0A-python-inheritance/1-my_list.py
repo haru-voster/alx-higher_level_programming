f7c9f19eef753aa60975a14a02815052f7a5a2ea
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-10 21:08:28 +0300

    my list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+my_list.py
+"""
+
+
+class MyList(list):
+    pass
+
+    def print_sorted(self):
+        """Methot that sorted a list"""
+        print(sorted(list(self)))
