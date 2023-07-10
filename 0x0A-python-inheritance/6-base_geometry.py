a6df504bf84c7d99e3c3a01ad395051820a04e55
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-10 21:25:20 +0300

    6-base_geometry.py classmethod

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+6-base_geometry.py
+haru-voster
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    @classMethod
+    def area(self):
+        """method for calculatING area"""
+
+        raise Exception("area() is not implemented")
