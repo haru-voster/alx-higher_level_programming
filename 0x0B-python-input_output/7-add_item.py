eb0a4e767c1473120d66eb1fde976e8c8105a8c2
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-11 19:25:56 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""ADD ARG."""
-
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
@@ -14,4 +12,4 @@ if __name__ == "__main__":
     except FileNotFoundError:
         items = []
     items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    save_to_json_file(items, "add_item.
