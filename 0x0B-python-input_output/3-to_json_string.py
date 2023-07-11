4d92f8a8a4d2119bd56a5578483a9ca3480e0781
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-11 18:31:39 +0300

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-"""JSON REP """
+
+import json
 
 
-def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number
-of characters written. Overwrites existing content.
-
-    Args:
-        filename (str): name of file to be opened
-        text (str): chars to be written
-
-    """
-    # with w flag existing file automatically replaced
-    with open(filename, 'w', encoding='utf-8') as file:
-        chars_written = file.write(text)
-        return chars_written
+def to_json_string(my_obj):
+    """function that returns he JSON representation of an object(string)"""
+    return json.dumps(my_obj)
