a40204ea5fe9dee69f6fe5d1aa69dd86f12e0adc
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-08-16 21:04:23 +0300

    cities of calfornia in sorted order

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- Lists all cities of calfornia in usa in a database
+-- Results are ordered by ascending cities.id.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
