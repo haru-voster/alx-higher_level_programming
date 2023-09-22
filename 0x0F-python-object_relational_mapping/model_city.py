a50b454eb267f314cd421921b1d3512a615cbdd6
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-09-22 21:04:15 +0300

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""Defines a city model
-    Inherits from the SQLAlchemy Base and links to the MYSQL cities table.
+
 """
-from sqlalchemy import Column, Integer, String, ForeignKey
+    this module contains a Base and City class
+"""
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
 
 
 Base = declarative_base()
 
 
 class City(Base):
-    """ Represents a city for a MYSQL database.
-    id (sqlalchemy.Integer): The cities id.
-    name (sqlalchemy.String): The name of the city.
-    state_id(sqlalchemy,Integer): Foreign key to states_id
     """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states_id"), nullable=False)
+        City class inherits the Base class
+        Attributes:
+            id (int)
+            name (string)
+            state_id (string)
+    """
+    __tablename__ = 'cities'
 
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
