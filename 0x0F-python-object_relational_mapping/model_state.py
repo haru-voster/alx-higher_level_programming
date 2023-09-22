071a8b993d602e816995321f2d9f2b4368d44c7a
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-09-22 21:05:38 +0300

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
+
 """
-    defines a state class and an instance Base = declarative_base():
-    state clas:
-        inherits from Base
-        links to the MYSQL tables states
-        class attribute id that represents a column of an auto-generated
-        unique integer, can't be null.
-        class attribute name that represents a column of a string with
-        maximum 128 characters and can't be null.
+    this module contains a Base and State class
 """
-from sqlalchemy import Column, Integer, String
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
 
 
 Base = declarative_base()
 
 
 class State(Base):
-    """ Represents a state for a MYSQL database.
-    __tablename__ (str): The name of a MYSQL table to store states
-    id (sqlalchemy.Integer): The Primary key(states id).
-    name (sqlalchemy.String): The name of the state.
     """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
+        State class inherits the Base class
+        Attributes:
+            id (int)
+            name (string)
+    """
+    __tablename__ = 'states'
 
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
