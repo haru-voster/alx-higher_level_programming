0e16c41b27ef1d74f31f07c306275793af62112d
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-09-22 21:24:46 +0300

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 """lists all city objects from the database hbtn_0e_6_usa"""
+
+
 import sys
-from model_state import State
+from model_state import Base, State
 from model_city import City
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-if __name__ == "__main__":
-    engine = create_engine(
-            "mysql+mysqldb://{}:{}@localhost/{}"
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-            pool_pre_ping=True)
-    Sessions = sessionmaker(bind=engine)
-    sessions = Sessions()
 
-    for city, state in sessions.query(City, State) \
-            .filter(City.state_id == State.id) \
-            .order_by(City.id):
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+
+    # create a session
+    session = Session()
+
+    # extract all cities in a state
+    cities = session.query(State, City) \
+                    .filter(State.id == City.state_id)
+
+    # print all states
+
+    for ci in cities:
+        print("{}: ({}) {}".format(ci.State.name, ci.City.id, ci.City.name))
+
+    session.close()
