c0deba7ea8b4acce246210e3f35a49ba974d68c4
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-09-22 21:25:40 +0300

    Update 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
-""" Updates the name of state object
-    with the name passed as argument from the DB
 """
+    A script that changes teh name of a State object in hbtn_0e_6_usa
+    name of State where id = 2 to New Mexico
+cript.
+"""
+
+
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-if __name__ == "__main__":
-    engine = create_engine(
-            "mysql+mysqldb://{}:{}@localhost/{}"
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-            pool_pre_ping=True)
-    Sessions = sessionmaker(bind=engine)
-    sessions = Session()
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-    state = sessions.query(State).filter_by(id=2).first()
-    state.name = "New Mexico"
-    sessions.commit()
+    Session = sessionmaker(bind=engine)
 
+    Base.metadata.create_all(engine)
+
+    # create a session
+    session = Session()
+
+    # fetch row to change
+    rename_state = session.query(State) \
+                          .filter(State.id == 2).first()
+    rename_state.name = 'New Mexico'
+    session.commit()
+
+    session.close()
