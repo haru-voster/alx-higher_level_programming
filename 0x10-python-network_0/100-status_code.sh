b32a53b2f887c1c98b7903db3d474cb2daee707f
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-09-29 13:25:26 +0300

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s -H "Content-Type: appliation/json" -d "$(cat "$2")" "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1"
