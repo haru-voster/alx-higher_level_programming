d26a72a94917c0cbdb76384900b76fdf971228a4
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-09-29 13:24:57 +0300

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -sX POST $1 -H "Content-Type: application/json" -d @$2 -L
-
+curl -s -H "Content-Type: appliation/json" -d "$(cat "$2")" "$1"
