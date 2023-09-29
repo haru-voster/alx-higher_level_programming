33a809481cac639175982fa621cdad511d887c0e
haru-voster <harounbravo2@gmail.com>
2023-09-29 10:18:12 +0000

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  sends a POST request to the passed URL, and displays the body of the response
-curl -s -d "email=hr@test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
