7dd8b4a84f345fd49e08bdc9af806e5c9d0e5d66
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-09-30 10:55:19 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status
-test web pages
-"""
-import urllib.request
-
+# haru-voster
+""" fetches https://alx-intranet.hbtn.io/status """
+import urllib.request as req
 
 if __name__ == "__main__":
-    """fetches https://intranet.hbtn.io/status"""
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        html_str = html.decode('utf-8')
-
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html_str))
+    with req.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        dt = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(dt)))
+        print("\t- content: {}".format(dt))
+        print("\t- utf8 content: {}".format(dt.decode("utf-8")))
