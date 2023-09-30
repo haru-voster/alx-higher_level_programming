48c4f3750c518a0138a863a7f560156fdc452756
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-09-30 11:15:20 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status using requests"""
+# haru-voster
+""" fetches https://intranet.hbtn.io/status using requests
+"""
+
 import requests
 
-
 if __name__ == "__main__":
-    """fetches https://intranet.hbtn.io/status using requests"""
-    r = requests.get('https://intranet.hbtn.io/status')
+    s = requests.get("https://alx-intranet.hbtn.io/status")
+
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(s.text)))
+    print("\t- content: {}".format(s.text))
