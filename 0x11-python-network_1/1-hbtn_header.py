6dc248b836dbdd79cf49e490d60d09a406638629
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-09-30 11:04:29 +0300

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/bin/bash
 # haru-voster
-""" sends a request to the URL and displays the value of the X-Request-Id"""
+""" Sends request to URL provided as command and display the
+    value of the X-Request-Id 
+"""
 from sys import argv
 import urllib.request as req
 
@@ -8,4 +10,4 @@ if __name__ == "__main__":
     url = argv[1]
 
     with req.urlopen(url) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+        print(dict(response.headers).get("X-Request-Id"
