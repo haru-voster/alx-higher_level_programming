21dbc8c8fad0f24353fc106b1ca57d8c81c50ca3
haru-voster <harounbravo2@gmail.com>
2023-10-01 16:43:07 +0000

    js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+# 11 biggest
+const listArg = process.argv.slice(2);
+if (listArg.length === 0 || listArg.length === 1) {
+  console.log(0);
+} else {
+  listArg.sort((a, b) => a - b);
+  console.log(listArg[listArg.length - 2]);
+}
