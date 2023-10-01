e4d906e654b54b4668af2aa6c591fdd0bc91a4df
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-10-01 20:45:44 +0300

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Takes my GitHub credentials and uses the GitHub API to display my id """
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
 
-from sys import argv
-import requests
-from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(argv[1], argv[2])
-    s = requests.get("https://api.github.com/user", auth=auth)
-    print(s.json().get("id"))
+    from requests import get
+    from sys import argv
+
+    r = get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(r.json().get('id'))
