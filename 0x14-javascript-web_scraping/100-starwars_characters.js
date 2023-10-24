43a70fdbc169aa0089e0d00f4e2ee736b94e00cd
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-10-24 13:14:08 +0300

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,25 @@
 #!/usr/bin/node
-// A script that prints all characters of a Star Wars movie
-
+//advanced
 const request = require('request');
-const film = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${film}`;
-request(url, (err, res, body) => {
-  if (!err) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (err, res, body) {
-        if (!err) {
-          console.log(JSON.parse(body).name);
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    const characters = content.characters;
+    // console.log(characters);
+    for (const character of characters) {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const names = JSON.parse(body);
+          console.log(names.name);
         }
       });
-    });
+    }
   }
 });
