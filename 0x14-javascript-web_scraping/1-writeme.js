8475100881b27ef7cd98a8daeda0ef0117f35cbe
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-10-24 12:59:28 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
+// haru-voster
 
-const r = require('r');
+const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
 
-const fileName = process.argv[2];
-const fileContent = process.argv[3];
-
-r.writeFile(fileName, fileContent, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-  }
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
