a06ae2b8ce516a93da7724b60a5b8bb2abb2306d
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-10-24 13:03:15 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,36 +1,21 @@
 #!/usr/bin/node
+//haru-voster
 const request = require('request');
-
-const movieId = process.argv[2];
-const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
-const fullUrl = baseUrl.concat(movieId);
-
-request(fullUrl, (error, response, body) => {
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    // Create a variable to store the number of characters processed
-    let charactersProcessed = 0;
-    // Create an 
-    const characterNames = [];
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (!error) {
-          const charName = JSON.parse(body).name;
-          // Add the character name to the array
-          characterNames.push(charName);
-        }
-        // Increment the charactersProcessed variable
-        charactersProcessed++;
-        // Check if all characters have been processed
-        if (charactersProcessed === characters.length) {
-          // Log the character names when all characters have been processed
-          characterNames.forEach((actor) => {
-            console.log(actor);
-          });
-        }
-      });
-    });
-  } else {
-    console.log(error);
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
