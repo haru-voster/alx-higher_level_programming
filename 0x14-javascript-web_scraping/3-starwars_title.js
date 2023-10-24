5fe9cbf5ac3a635ed65e43da6261933657564414
haru-voster <harounbravo2@gmail.com>
2023-10-24 01:10:11 -0700

    title of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+//haru-voster
+
+const request = require('request');
+const movieId = process.argv[2];
+const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const fullUrl = baseUrl.concat(movieId);
+
+request(fullUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
+
